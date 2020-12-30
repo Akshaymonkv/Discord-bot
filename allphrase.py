c5decce269bd7dfe7efec4687fb21ab0188acf20
Akshaymon <kvakshaymon@gmail.com>
2020-12-30 06:04:25 +0000

    Initial commit

---
+++ allphrase.py
@@ -0,0 +1,67 @@
+list_2 = [
+  'Apex','apex','legends','Legends'
+]
+sad_words = [
+  "sad",
+  "depressed",
+  "unhappy",
+  "angry",
+  "miserable",
+  "depressing",
+  "bitter",
+  "dismal",
+  "heartbroken",
+  "melancholy",
+  "mournful",
+  "pessimistic",
+  "somber",
+  "sorrowful",
+  "sorry",
+  "wistful",
+  "bereaved",
+  "cheerless",
+  "dejected",
+  "despairing",
+  "despondent",
+  "gloomy",
+  "heartsick",
+  "heavyhearted",
+  "low",
+  "low-spirited",
+  "sick at heart",
+  "troubled",
+  "weeping"
+  ]
+initial = [
+  "Cheer up!",
+  "Hang in there.",
+  "You are a great person / bot!"
+  ]
+brooklyn_99_quotes = [
+        'I\'m the human form of the 💯 emoji.',
+        'Bingpot!',
+        (
+            'Cool. Cool cool cool cool cool cool cool, '
+            'no doubt no doubt no doubt no doubt.'
+        ),
+    ]
+apex_legend_quotes = ['Gwan,'
+  'try to stop me.',
+  'Let’s get the job done.',
+  'We’re in this together, remember dat.',
+  'Pay attention, might learn a ting or two.',
+  'This is gonna be a good fight.',
+  'It’s only over when it’s over.',
+  'Memba dis face, it’s comin’ for ya.'
+]
+
+anime_search = ['https://www.imdb.com/title/tt2560140/?ref_=kw_li_tt',
+'https://www.imdb.com/title/tt0417299/?ref_=kw_li_tt',
+'https://www.imdb.com/title/tt3398540/?ref_=kw_li_tt',
+'https://www.imdb.com/title/tt5626028/?ref_=kw_li_tt',
+'https://www.imdb.com/title/tt1695360/?ref_=kw_li_tt',
+'https://www.imdb.com/title/tt9335498/?ref_=kw_li_tt',
+'https://www.imdb.com/title/tt2098220/?ref_=kw_li_tt',
+'https://www.imdb.com/title/tt0877057/?ref_=kw_li_tt',
+'https://www.imdb.com/title/tt12343534/?ref_=kw_li_tt',
+'https://www.imdb.com/title/tt0988824/?ref_=kw_li_tt']
---
+++ main.py
@@ -0,0 +1,138 @@
+import discord #importing the discord library
+import os
+import requests #impoorting an html request function
+import json #importing json to work with api
+import random #to ensure bot replies randomly 
+from replit import db  #to import the repl database
+from keepalive import keep_alive #importing an html client
+from dotenv import load_dotenv
+import allphrase
+from allphrase import *
+
+client = discord.Client()
+
+#making sure that the bot responds
+if "responding" not in db.keys():
+  db["responding"] = True
+
+#function to collect quotes from external api
+def find_quote():
+  response = requests.get("https://zenquotes.io/api/random")
+  json_data = json.loads(response.text)
+  quote = json_data[0]['q'] + " -" + json_data[0]['a']
+  return(quote)
+
+
+
+#function to enable users to add cheering words
+def update_encouragements(encouraging_message):
+  if "encouragements" in db.keys():
+    encouragements = db["encouragements"]
+    encouragements.append(encouraging_message)
+    db["encouragements"] = encouragements
+  else:
+    db["encouragements"] = [encouraging_message]
+
+#function to enable users to delete cheering words
+def delete_encouragment(index):
+  encouragements = db["encouragements"]
+  if len(encouragements) > index:
+    del encouragements[index]
+    db["encouragements"] = encouragements
+#the event to check if the bot's ready
+@client.event
+async def on_ready():
+  print('{0.user} joined the party!'.format(client))
+
+@client.event
+async def on_member_join(member):
+  await member.channel.send(f'{member.mention} has joined the server!')
+
+
+#event when a message is received
+@client.event
+async def on_message(message):
+  if message.author == client.user:
+    return
+
+  msg = message.content
+
+
+  if message.content == '99!':
+    response = random.choice(brooklyn_99_quotes)
+    await message.channel.send(response)
+  if 'suggest me an anime to watch' in message.content.lower():
+    response = random.choice(anime_search)
+    await message.channel.send(response+' {0.author.mention}'.format(message))
+  if 'latest news' in message.content.lower():
+    response =  NewsFromBBC()  
+    await message.channel.send(response)
+  
+
+
+  if 'happy birthday' in message.content.lower():
+    await message.channel.send('Happy Birthday! 🎈🎉')
+  if 'christmas' in message.content.lower():
+    await message.channel.send('Happy Christmas! 🎈🎉')
+  if  msg.startswith('hi'):
+    k = 'Hi {0.author.mention}'.format(message)
+    await message.channel.send(k)
+  if 'hello' in message.content.lower():
+    k = 'Hi {0.author.mention}'.format(message)
+    await message.channel.send(k)
+  if 'how are you?' in message.content.lower():
+    k = 'Fine. What about you? {0.author.mention}'.format(message)
+    await message.channel.send(k)  
+  if msg.startswith('$inspireme'):
+    quote = find_quote()
+    await message.channel.send(quote + '  {0.author.mention}'.format(message))
+
+  if db["responding"]:
+    options = initial
+    if "encouragements" in db.keys():
+      options = options + db["encouragements"]
+
+    if any(word in msg for word in sad_words):
+      await message.channel.send(random.choice(options))
+
+    if any(word in msg for word in list_2):
+      await message.channel.send(random.choice(apex_legend_quotes) + ' {0.author.mention}'.format(message))
+
+
+  #to let user add new cheer words and add them to db
+  if msg.startswith("$newmsg"):
+    encouraging_message = msg.split("$newmsg ",1)[1]
+    update_encouragements(encouraging_message)
+    await message.channel.send("New encouraging message added.")
+
+
+  #to let user delete cheer word from database
+  if msg.startswith("$del"):
+    encouragements = []
+    if "encouragements" in db.keys():
+      index = int(msg.split("$del",1)[1])
+      delete_encouragment(index)
+      encouragements = db["encouragements"]
+    await message.channel.send(encouragements)
+
+
+  #This is to show a list of cheer words in the db
+  if msg.startswith("$list"):
+    encouragements = []
+    if "encouragements" in db.keys():
+      encouragements = db["encouragements"]
+    await message.channel.send(encouragements)
+
+  if msg.startswith("$responding"):
+    value = msg.split("$responding ",1)[1]
+
+    if value.lower() == "true":
+      db["responding"] = True
+      await message.channel.send("Responding is on.")
+    else:
+      db["responding"] = False
+      await message.channel.send("Responding is off.")
+
+keep_alive()
+client.run(os.getenv('TOKEN'))
+
